64462fc3b88b8cd99d118a64f7b36bbcbc9f021f
Sky Link <s.z_link@yahoo.com>
2017-06-26 16:31:09 -0600

    Methods to scan for specific hands in Hand class.

--- src/edu/cnm/deepdive/cards/Hand.java
+++ src/edu/cnm/deepdive/cards/Hand.java
@@ -31,10 +31,65 @@ public class Hand implements Comparable<Hand> {
 
   @Override
   public int compareTo(Hand arg0) {
+
     // TODO Auto-generated method stub
     return 0;
   }
   
   
+  public Card[][] byRanks() {
+    Card[][] table = new Card[Card.Rank.values().length][];
+    for (Card.Rank rank: Card.Rank.values()) {
+      ArrayList<Card> members = new ArrayList<>();
+      for (Card card: cards) {
+        if (card.rank == rank) {
+          members.add(card);
+        }
+      }
+      members.sort(null);
+      table[rank.ordinal()] = members.toArray(new Card[] {});
+    }
+    return table;
+  }
   
+  public Card[][] bySuits() {
+    Card[][] table = new Card[Card.Suit.values().length][];
+    for (Card.Suit suit: Card.Suit.values()) {
+      ArrayList<Card> members = new ArrayList<>();
+      for (Card card: cards) {
+        if (card.suit == suit) {
+          members.add(card);
+        }
+      }
+      members.sort(null);
+      table[suit.ordinal()] = members.toArray(new Card[] {});
+    }
+    return table;
+  }
+  
+  
+  private boolean flush(Card[][] table) {
+    for (Card[] suitedCards: table) {
+      if(suitedCards.length > 0 && suitedCards.length < size) {
+        return false;
+      }
+      if(suitedCards.length == size) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  private ArrayList<Card.Rank> sets(Card[][] table, int size) {
+    ArrayList<Card.Rank> result = new ArrayList<>();
+    for (Card.Rank rank : Card.Rank.values()) {
+      if (table[rank.ordinal()].length == size) {
+        result.add(rank);
+      }
+    }
+    return result;
+  }
+  
+  
+
 }
